81630539ff7124dae4bdc4e455b7a5d2f36b6fa2
Souravdey777 <piyush.kolkata@gmail.com>
2019-04-26 13:49:59 +0530

    Updates

---
+++ precache-manifest.0d5fa92167df30c97876925d920c87b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc28cbcfbefb7bd1d23261ae7f0d8e6a",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "06f71f14efee5ad19939",
+    "url": "/todo-app/static/css/main.45bab136.chunk.css"
+  },
+  {
+    "revision": "f04ec492e7e4ab42679b",
+    "url": "/todo-app/static/js/2.fed26185.chunk.js"
+  },
+  {
+    "revision": "06f71f14efee5ad19939",
+    "url": "/todo-app/static/js/main.c52dd92b.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
